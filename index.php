9b44d02ce488c94fef442d9e15e6437e50352ff9
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2020-01-19 14:28:07 -0500

    Added Webpack and Typescript to project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spelling Tutor</title>
+</head>
+<body>
+    <header>Spelling Tutor</header>
+    <main>
+        <input type="text" id="playerinput">
+        <button id="repeat">Repeat Word</button>
+    </main>
+    <footer>Created by Josh Byvelds in 2020 - Version 0.0.0 - (--env.major --env.minor --env.patch)</footer>
+    <script src="dist/bundle.js"></script>
+</body>
+</html>
